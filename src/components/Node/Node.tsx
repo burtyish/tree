37c03e597c0c28adf75a4d63ac5ce0c71e4c08bc
yishai <yishai@gmail.com>
2023-01-28 22:18:24 +0200

    persist node expansion state

--- src/components/Node/Node.tsx
+++ src/components/Node/Node.tsx
@@ -1,41 +1,62 @@
-import { useState } from 'react';
 import { useNodesData } from '../../api/hooks/apiHooks';
 import { NodeType, TreeNode } from '../../types/NodeData';
 
-export function Node({ node }: { node: TreeNode }) {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  function toggleExpand() {
-    setIsExpanded((state) => !state);
-  }
-
+export function Node({
+  node,
+  isExpanded,
+  onToggleExpand,
+}: {
+  node: TreeNode;
+  isExpanded: (id: string) => boolean;
+  onToggleExpand: (id: string) => void;
+}) {
+  const id = node.item.id;
   return (
     <li>
       {'children' in node && node.children.length > 0 ? (
-        <button onClick={toggleExpand}>{isExpanded ? '[-]' : '[+]'}</button>
+        <button onClick={() => onToggleExpand(id)}>
+          {isExpanded(id) ? '[-]' : '[+]'}
+        </button>
       ) : (
         <>[ ]</>
       )}
 
       {`${node.item.type}: ${node.item.name}`}
-      {'children' in node && node.children.length > 0 && isExpanded && (
+      {'children' in node && node.children.length > 0 && isExpanded(id) && (
         <NodeChildren
           ids={node.children.map((child) => child.id)}
           type={node.children[0].type}
+          onToggleExpand={onToggleExpand}
+          isExpanded={isExpanded}
         />
       )}
     </li>
   );
 }
 
-function NodeChildren({ type, ids }: { type: NodeType; ids: string[] }) {
+function NodeChildren({
+  type,
+  ids,
+  isExpanded,
+  onToggleExpand,
+}: {
+  type: NodeType;
+  ids: string[];
+  isExpanded: (id: string) => boolean;
+  onToggleExpand: (id: string) => void;
+}) {
   const { data, status } = useNodesData(type, ids);
 
   if (status === 'success') {
     return (
       <ul>
         {Object.entries(data).map(([id, child]) => (
-          <Node key={child.item.id} node={child} />
+          <Node
+            key={child.item.id}
+            node={child}
+            onToggleExpand={onToggleExpand}
+            isExpanded={isExpanded}
+          />
         ))}
       </ul>
     );
--- src/components/Tree/Tree.tsx
+++ src/components/Tree/Tree.tsx
@@ -1,14 +1,40 @@
+import { useCallback, useState } from 'react';
 import { useNodesData } from '../../api/hooks/apiHooks';
 import { Node } from '../Node/Node';
 
-export function Tree({}: {}) {
-  const { data, status } = useNodesData('connection');
+export function Tree() {
+  const [nodeIdExpanded, setNodeExpanded] = useState<
+    Record<string /*id*/, boolean>
+  >({});
+  const toggleExpanded = useCallback((id: string) => {
+    setNodeExpanded((state) => {
+      if (!state[id]) {
+        return { ...state, [id]: true };
+      } else {
+        const newState = { ...state };
+        delete newState[id];
+        return newState;
+      }
+    });
+  }, []);
+  const isNodeExpanded = useCallback(
+    (id: string) => {
+      return !!nodeIdExpanded[id];
+    },
+    [nodeIdExpanded]
+  );
+  const { data, status } = useNodesData('connection', []);
 
   if (status === 'success') {
     return (
       <ul>
         {Object.entries(data).map(([_id, connection]) => (
-          <Node key={connection.item.id} node={connection} />
+          <Node
+            key={connection.item.id}
+            node={connection}
+            onToggleExpand={toggleExpanded}
+            isExpanded={isNodeExpanded}
+          />
         ))}
       </ul>
     );
