c1e78406c5344dc62fa4926b9ab620905848d36c
yishai <yishai@gmail.com>
2023-01-31 15:48:23 +0200

    prevent mutating ids parameter

--- src/api/hooks/apiHooks.tsx
+++ src/api/hooks/apiHooks.tsx
@@ -3,8 +3,8 @@ import { GenericNode, NodeType } from '../../types/NodeData';
 import { getData } from './networkClient';
 import { nodeTypePluralMap } from './nodePlurals';
 
-export function useNodesData<T extends NodeType>(type: T, ids: string[] = []) {
-  const queryKey = [nodeTypePluralMap[type], ids.sort()];
+export function useNodesData<T extends NodeType>(type: T, ids: readonly string[] = []) {
+  const queryKey = [nodeTypePluralMap[type], [...ids].sort()];
   const url = new URL(`https://mockserver.fake/api/${nodeTypePluralMap[type]}`);
   ids.forEach((nodeId) => url.searchParams.append('id', nodeId));
 
